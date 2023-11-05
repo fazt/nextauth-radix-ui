dab4ff758df57196fdc4aa98810b8aa7f4de4a89
Fazt <fazt@faztweb.com>
2023-11-05 17:44:58 -0500

    first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@radix-ui/themes";
+import { BookmarkIcon } from "@radix-ui/react-icons";
+
+function LoginPage() {
+  return (
+    <div>
+      LoginPage
+      <Button>
+        <BookmarkIcon width="16" height="16" /> Bookmark
+      </Button>
+    </div>
+  );
+}
+export default LoginPage;
