b024f7841926dff719594e95266ec87645b4faa8
Fazt <fazt@faztweb.com>
2023-11-05 19:04:51 -0500

    prisma added

--- src/components/auth/SignupForm.tsx
+++ src/components/auth/SignupForm.tsx
@@ -6,6 +6,7 @@ import {
   PersonIcon,
 } from "@radix-ui/react-icons";
 import { useForm, Controller } from "react-hook-form";
+import axios from 'axios'
 
 function SigninForm() {
   const { control, handleSubmit } = useForm({
@@ -16,8 +17,10 @@ function SigninForm() {
     }
   });
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data);
+  const onSubmit = handleSubmit(async (data) => {
+    const res = await axios.post('/api/auth/register', data)
+    console.log(res)
+
   });
 
   return (
